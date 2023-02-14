2f59b4225c94d7f64814b6095971be7f6c6ad4d1
Lost004 <nazimri@code.edu.com>
2023-02-14 13:07:04 -0800

    14.02.23

---
+++ ExtentıonMethods.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace _14_02_23
+{
+	internal static class ExtentıonMethods
+	{
+		public static bool IsContainsDigit(this string str)
+		{
+			if (str == null) return false;
+			foreach (var cr in str)
+			{
+				if (char.IsDigit(cr))
+					return true;
+			}
+			return false;
+		}
+		public static string ToCapitalize(this string str)
+		{
+			string newStr = Char.ToUpper(str[0]) + str.Substring(1).ToLower();
+			return newStr;
+		}
+		public static bool IsOdd(this int num1)
+		{
+			if (num1 % 2 != 0) return true;
+			else
+			{
+				return false;
+			}
+		}
+		public static bool IsEven (this int num2)
+		{
+			if (num2 % 2 == 0) 
+				return true;
+			else
+			{
+				return false;
+			}
+		}
+			
+
+
+	}
+}
---
+++ Stuent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace _14_02_23
+{
+	internal class Stuent
+	{public static bool CheckNo(string groupNo)
+		{
+			if (groupNo == null)
+				return false;
+			if (groupNo.Length != 4)
+				return  false;
+			if (char.IsUpper(groupNo[0]) && char.IsDigit(groupNo[1])&& char.IsDigit(groupNo[2]) && char.IsDigit(groupNo[3]))
+			return true;
+
+			return false;
+		}
+	     
+
+		public string Fullname;
+		public byte GroupNo;
+		public int Age;
+
+		
+	}
+}
